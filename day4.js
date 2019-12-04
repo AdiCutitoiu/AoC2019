39a589e71e782aaaf6680bdaf2bc6532cf01510c
AdiCutitoiu <acutitoiu2@gmail.com>
2019-12-04 09:47:18 +0200

    Create day4.js

---
+++ day4.js
@@ -0,0 +1,44 @@
+const FROM = 278384;
+const TO = 824795;
+
+function check(number, filter) {
+  const digits = ("" + number).split("");
+
+  const orderedDigits = [...digits].sort();
+  if (digits.join("") !== orderedDigits.join("")) {
+    return false;
+  }
+
+  const freqs = digits
+    .reduce(
+      (acc, current) => ({
+        ...acc,
+        [current]: (acc[current] || 0) + 1
+      }),
+      {}
+    );
+
+  const matchingValues = Object.keys(freqs)
+    .map(key => freqs[key])
+    .filter(filter);
+
+  return matchingValues.length !== 0;
+}
+
+let count = 0;
+for (let i = FROM; i <= TO; i++) {
+  if (check(i, value => value >= 2)) {
+    count++;
+  }
+}
+
+console.log(count);
+
+count = 0;
+for (let i = FROM; i <= TO; i++) {
+  if (check(i, value => value === 2)) {
+    count++;
+  }
+}
+
+console.log(count);
